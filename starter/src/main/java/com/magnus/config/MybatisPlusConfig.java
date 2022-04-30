dd476d347df9024ffd4605a386ea76cbac550afb
Qomolang <84028136@qq.com>
2022-04-30 19:32:51 +0800

    1.增加mybatis及mysql数据源 2.增加拦截器 3.增加controller层异常兜底

---
+++ starter/src/main/java/com/magnus/config/MybatisPlusConfig.java
@@ -0,0 +1,11 @@
+package com.magnus.config;
+
+//import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+//@MapperScan("com.magnus.service.mapper")
+public class MybatisPlusConfig {
+
+}
