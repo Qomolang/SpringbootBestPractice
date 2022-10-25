021e472f627661181991681b8ebde78a0c11ec12
Qomolang <84028136@qq.com>
2022-10-25 23:02:48 +0800

    导入导出 完善结果

--- practice-project-excel-import-export/src/main/java/com/magnus/excel/biz/UserExcelBizService.java
+++ practice-project-excel-import-export/src/main/java/com/magnus/excel/biz/UserExcelBizService.java
@@ -8,28 +8,63 @@ public class UserExcelBizService {
     /**
      * 同步导出（模板）
      */
-    public void exportAsync(Long tenantId, Long userId){
+    public void exportSync(Long tenantId, Long userId){
 
         //1, 校验权限
 
-        //2. 校验是否已经有导出，任务类型+唯一键确认
+        //2. 校验是否已经正在导出，任务类型+唯一键确认
 
-        //3.
+        //3. 获取数据库中的records
 
+        //4. 数据库records -> ExcelEntity
+
+        //5. ExcelEntity -> File Stream
+
+        //6. File Stream -> URL
+
+        //7. 返回结果
     }
 
     /**
      * 异步导出
      */
-    public void exportSync(Long tenantId, Long userId){
+    public void exportAsync(Long tenantId, Long userId){
+
+        //1, 校验权限
+
+        //2. 校验是否已经正在导出，任务类型+唯一键确认
+
+        //3. 获取数据库中的records
+
+        //4. 数据库records -> ExcelEntity
+
+        //5. ExcelEntity -> File Stream
+
+        //6. File Stream -> URL
+
+        //7. 返回结果
+    }
+
+    /**
+     * 轮询检查异步导出结果
+     */
+    public void pollExportAsyncResult(Long tenantId, Long userId){
 
     }
 
     /**
      * 异步导入
      */
-    public void importSync(Long tenantId, Long userId){
+    public void importAsync(Long tenantId, Long userId){
 
     }
+
+    /**
+     * 轮询检查异步导入结果
+     */
+    public void pollImportAsyncResult(Long tenantId, Long userId){
+
+    }
+
 
 }
