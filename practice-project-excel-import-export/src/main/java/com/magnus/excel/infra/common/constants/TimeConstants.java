2064c6db561800da62aa80d1bf4b66e88c80bbfa
Qomolang <84028136@qq.com>
2023-01-07 23:39:06 +0800

    导入导出模块 完善缓存部分

---
+++ practice-project-excel-import-export/src/main/java/com/magnus/excel/infra/common/constants/TimeConstants.java
@@ -0,0 +1,14 @@
+package com.magnus.excel.infra.common.constants;
+
+/**
+ * @author gaosong
+ */
+public class TimeConstants {
+
+    public static final Integer ONE_MINUTE_SECONDS = 60;
+
+    public static final Integer ONE_HOUR_SECONDS = 3600;
+
+    public static final Integer ONE_DAY_SECONDS = 86400;
+
+}
