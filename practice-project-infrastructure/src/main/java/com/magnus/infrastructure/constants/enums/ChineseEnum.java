94e954d898ecad3bbcf5c62d3a6d907bc5bca8c4
gaosong <84028136@qq.com>
2023-10-22 23:20:03 +0800

    infra中增加枚举的例子

---
+++ practice-project-infrastructure/src/main/java/com/magnus/infrastructure/constants/enums/ChineseEnum.java
@@ -0,0 +1,20 @@
+package com.magnus.infrastructure.constants.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ChineseEnum {
+    /**
+     * 例子
+     */
+    EXAMPLE(0, "例子"),
+    ;
+    @EnumValue
+    private int value;
+
+    private String desc;
+
+}
